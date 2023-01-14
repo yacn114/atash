f057985699f407ff0557df5e3ad7d35ea70b006b
yacn1414 <yacn1214@gmail.com>
2023-01-14 22:58:54 +0330

    first commit

---
+++ home/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here.
+class SiteInformation(models.Model):
+    siteNamePersian = models.CharField(max_length=40,verbose_name="اسم سایت فارسی")
+    logo = models.ImageField(upload_to='images/logo',verbose_name="لوگو سایت")
+    siteNameEnglish = models.CharField(max_length=40,verbose_name="اسم سایت انگلیسی")
+    class Meta:
+        verbose_name = 'اطلاعات سایت'
+        verbose_name_plural = 'اطلاعات سایت'
+    def _str__(self):
+        return self.siteNameEnglish
+class Categories(models.Model):
+    title = models.CharField(max_length=20,verbose_name="اسم دسته بندی")
+    class Meta:
+        verbose_name = "دسته بندی"
+        verbose_name_plural = "دسته بندی ها"
+    def __str__(self):
+        return self.title
+    
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    caption = models.TextField()
+    image = models.ImageField(upload_to="post/")
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    Categories = models.ManyToManyField(Categories)
+    status = models.BooleanField(default=False)
+    class Meta:
+        verbose_name = "پست"
+        verbose_name_plural = "پست ها"
+    def __str__(self):
+        return self.title
---
+++ home/templatetags/information.py
@@ -0,0 +1,11 @@
+from home.models import SiteInformation
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def information(inp):
+    if inp == "name":
+        return SiteInformation.objects.all()[0].siteNamePersian
+    if inp == "nameE":
+        return SiteInformation.objects.all()[0].siteNameEnglish
